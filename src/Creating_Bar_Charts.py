f23019d5eba7b5dc07199c5f18a376b8a4005b3b
Brynn Dafoe <dafoe@usc.edu>
2025-12-06 18:15:07 -0800

    edited results

--- src/Creating_Bar_Charts.py
+++ src/Creating_Bar_Charts.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import sys
 import matplotlib.pyplot as plt
 
 # need to access config from root
@@ -34,6 +35,32 @@ def Ranked_Results_Bar_Chart(gender: str):
         plt.ylabel('Skier')
         plt.title("Men's Singles Moguls: Ranked by Probability")
         plt.xlim(0,.7)
+        plt.grid(axis='x', linestyle='--', alpha=0.5)
+
+        plt.tight_layout()
+        plt.show()
+    elif gender == "women":
+        ranked_path = os.path.join(DATA_FOLDER, TESTING_DATA_RANKED_WOMEN)
+        ranked_df = pd.read_csv(ranked_path)
+
+        ranked_df.columns = ranked_df.columns.str.strip()
+
+        df_sorted = ranked_df.sort_values('predicted_prob_top5', ascending=False).head(10)
+        # df_sorted = ranked_df.sort_values('predicted_prob_top5', ascending=False)
+            # IF WANT TO SEE ALL ATHLETES, USE THIS LINE ^^^ INSTEAD!
+        df_sorted = df_sorted.iloc[::-1]
+
+        colors = ['#b5e2ff' if i < 5 else '#45b6fe' for i in range(len(df_sorted))]
+
+        plt.rcParams['font.size'] = 10
+
+        plt.figure(figsize=(8,12))
+        plt.barh(df_sorted['Name'], df_sorted['predicted_prob_top5'], color=colors)
+
+        plt.xlabel('Predicted Probability')
+        plt.ylabel('Skier')
+        plt.title("Women's Singles Moguls: Ranked by Probability")
+        plt.xlim(0,.9)
         plt.grid(axis='x', linestyle='--', alpha=0.5)
 
         plt.tight_layout()
